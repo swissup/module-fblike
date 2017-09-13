6a458a309e346c390aa7ebb5890e9b1a69762ab9
Vitaliy <vitaliy@mage2cloud.com>
2017-09-13 16:08:06 +0300

    fix JSCS

--- view/frontend/web/js/fblike.js
+++ view/frontend/web/js/fblike.js
@@ -1,7 +1,6 @@
 define([
     'jquery'
 ], function ($) {
-
     'use strict';
 
     $.widget('swissup.fblike', {
@@ -11,17 +10,26 @@ define([
             locale: 'en_US'
         },
 
+        /**
+         * widget constructor
+         */
         _create: function () {
             this._loadSDK(this.initializeButtons.bind(this));
         },
 
+        /**
+         * [_loadSDK description]
+         * @param  {Function} fbAsyncCallback
+         */
         _loadSDK: function (fbAsyncCallback) {
             var script;
+
             if (typeof FB == 'undefined') {
                 script = document.createElement('script');
                 script.type = 'text/javascript';
                 script.src = '//connect.facebook.net/' + this.options.locale + '/sdk.js';
                 script.id = 'facebook-jssdk';
+
                 if (fbAsyncCallback) {
                     window.fbAsyncInit = fbAsyncCallback;
                 }
@@ -31,33 +39,41 @@ define([
             }
         },
 
+        /**
+         * [initializeButtons description]
+         */
         initializeButtons: function () {
             FB.init({
-                appId : this.options.appId,
-                xfbml : true,
-                version : 'v2.10'
+                appId: this.options.appId,
+                xfbml: true,
+                version: 'v2.10'
             });
             this.addObservers();
         },
 
+        /**
+         * Add click observer for custom like button
+         */
         addObservers: function () {
             $('.fbl-custom .like').each(function () {
-                if ($(this).hasClass("initialized")) {
+                if ($(this).hasClass('initialized')) {
                     return;
                 }
                 $(this).click(function () {
                     // variable 'this' - element with the observer
                     // call fb dialog to like product
+                    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
                     FB.ui(
                         {
                             method: 'share_open_graph',
                             action_type: 'og.likes',
                             action_properties: JSON.stringify({
-                                object: $(this).data('url'),
+                                object: $(this).data('url')
                             })
                         },
                         function (response) {}
                     );
+                    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
                 });
                 $(this).addClass('initialized');
             });
